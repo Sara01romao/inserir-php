e70a49a8c63d7ffd025aa91d4f3e748f79f51c86
Sara01romao <sara-romao@live.com>
2020-11-22 23:56:29 -0300

    .

--- index.php
+++ index.php
@@ -2,13 +2,72 @@
 <html lang="en">
 <head>
 <?php include_once 'view/dependencias.php'; ?>
+	<?php include_once 'view/menu.php'; ?>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Home</title>
 </head>
 <body>
-  <?php
-  include_once 'view/menu.php';
-  ?>
+
+<br>
+<div class="card" style="margin: 0px 50px;">
+  <div class="card-header">
+    Venda
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Special title treatment</h5>
+    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <a href="#" class="btn btn-primary" style="margin-right: 20px;" >Histrico de Vendas</a>
+    <a href="view/cadastrar_venda2.php" class="btn btn-primary">Cadastrar Venda</a>
+  </div>
+</div>
+<br>
+
+<br>
+<div class="card" style="margin: 0px 50px;">
+  <div class="card-header">
+    Caixa
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Special title treatment</h5>
+    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <a href="#" class="btn btn-primary" style="margin-right: 20px;" >Histrico de Vendas</a>
+    <a href="view/cadastrar_venda2.php" class="btn btn-primary">Cadastrar Venda</a>
+  </div>
+</div>
+<br>
+
+<br>
+<div class="card" style="margin: 0px 50px;">
+  <div class="card-header">
+    Clientes
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Special title treatment</h5>
+    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <a href="#" class="btn btn-primary" style="margin-right: 20px;" >Histrico de Vendas</a>
+    <a href="view/cadastrar_venda2.php" class="btn btn-primary">Cadastrar Venda</a>
+  </div>
+</div>
+<br>
+
+<div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="..." alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+
+
+
+
+
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
 </body>
 </html>
